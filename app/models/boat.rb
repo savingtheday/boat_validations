ad2fdbdac1be34cacd6be749c7142b5c4e63b7e2
Kris <weldon.kris@gmail.com>
2016-03-11 15:40:57 -0500

    Sign in and out

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,6 @@ class Boat < ActiveRecord::Base
   has_many :jobs
   belongs_to :user
   validates_uniqueness_of :bname
+  validates :location, inclusion: { in: %w(PHL NYC CAM),
+    message: "%{value} is not valid." }
 end
