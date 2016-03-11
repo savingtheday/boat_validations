70f239871bf0c9a599ee95315a587b490f6b8db5
Kris <weldon.kris@gmail.com>
2016-03-11 15:26:26 -0500

    Validations added

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,4 +2,9 @@ class Job < ActiveRecord::Base
   belongs_to :boat
   validates_uniqueness_of :job_name
   validates :job_cost, :numericality => {greater_than_or_equal_to: 1000 }
+  validates :job_name, length: { minimum: 50 }
+  validates :origin, inclusion: { in: %w(PHL NYC CAM),
+    message: "%{value} is not valid." }
+  validates :origin, inclusion: { in: %w(PHL NYC CAM),
+    message: "%{value} is not valid." }
 end
